3876959dc86e93dee84930e1fce096551829167d
nishaj0 <njnishaj0@gmail.com>
2024-01-07 11:30:00 +0530

    feat(client): created header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home, Profile, SignIn, About, SignUp } from "./pages";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home, Profile, SignIn, About, SignUp } from './pages';
+import { Header } from './components';
 
 function App() {
-     return (
-          <BrowserRouter>
-               <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/sign-in" element={<SignIn />} />
-                    <Route path="/sign-up" element={<SignUp />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/profile" element={<Profile />} />
-               </Routes>
-          </BrowserRouter>
-     );
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
