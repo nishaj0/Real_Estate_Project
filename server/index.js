779cecc1f5e4bbc0904be5b196ad136f2a84ae14
nishaj0 <njnishaj0@gmail.com>
2024-01-07 13:42:57 +0530

    feat(server): connected to DB

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// connect to DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to DB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
